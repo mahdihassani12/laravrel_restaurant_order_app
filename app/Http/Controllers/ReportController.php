d7c847811f2df154fe1496709d5cc94c1766cb07
ziayamin <ziayamin8@gmail.com>
2021-11-21 09:48:41 +0430

    add search to all seactions in daily

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -829,46 +829,56 @@ class ReportController extends Controller
 
                         $data = DB::table('inside_order as io')
                             ->join('menu', 'menu.menu_id', '=', 'io.menu_id')
+                            ->join('inside_order_total as iot', 'iot.order_id', '=', 'io.total_id')
                             ->select( 'menu.name as menu_name',  DB::raw("SUM(order_amount) as or_am"), 'io.order_id',DB::raw("SUM(io.price*io.order_amount) as total_price"),
                                 'io.price','io.total_id','io.created_at')
                             ->whereDate('io.created_at','=',$date)
                             ->where('menu.category_id',$request->menu)
+                            ->where('iot.identity','=',$search)
                             ->groupBy('io.created_at','menu.menu_id')
                             ->orderByDesc('or_am')
                             ->get();
                         $sum = DB::table('inside_order as io')
                             ->join('menu', 'menu.menu_id', '=', 'io.menu_id')
+                            ->join('inside_order_total as iot', 'iot.order_id', '=', 'io.total_id')
                             ->whereDate('io.created_at','=',$date)
                             ->where('menu.category_id',$request->menu)
+                            ->where('iot.identity','=',$search)
                             ->sum(DB::raw('io.price*order_amount'));
 
                         $discount= DB::table('inside_order_total as io')
                             ->join('inside_order','inside_order.total_id','=','io.order_id')
                             ->join('menu', 'menu.menu_id', '=', 'inside_order.menu_id')
                             ->where('menu.category_id',$request->menu)
+                            ->where('io.identity','=',$search)
                             ->whereDate('io.created_at','>=',$date)
                             ->sum('discount');
 
                         //outside orders
                         $data_out = DB::table('outside_order as io')
                             ->join('menu', 'menu.menu_id', '=', 'io.menu_id')
+                            ->join('outside_order_total as oot', 'oot.order_id', '=', 'io.total_id')
                             ->select( 'menu.name as menu_name',  DB::raw("SUM(order_amount) as or_am"),
                                 'io.price','io.total_id','io.created_at')
                             ->groupBy('io.created_at','menu.menu_id')
                             ->whereDate('io.created_at','=',$date)
                             ->where('menu.category_id',$request->menu)
+                            ->where('oot.identity','=',$search)
                             ->orderByDesc('or_am')
                             ->get();
                         $sum_out = DB::table('outside_order as io')
                             ->join('menu', 'menu.menu_id', '=', 'io.menu_id')
+                            ->join('outside_order_total as oot', 'oot.order_id', '=', 'io.total_id')
                             ->whereDate('io.created_at','=',$date)
                             ->where('menu.category_id',$request->menu)
+                            ->where('oot.identity','=',$search)
                             ->sum(DB::raw('io.price*order_amount'));
 
                         $discount_out= DB::table('inside_order_total as io')
                             ->join('inside_order','inside_order.total_id','=','io.order_id')
                             ->join('menu', 'menu.menu_id', '=', 'inside_order.menu_id')
                             ->whereDate('io.created_at','=',$date)
+                            ->where('io.identity','=',$search)
                             ->where('menu.category_id',$request->menu)
                             ->sum('discount');
                         return response([
